39d8386699483bbc129d11a8f5e13e5d142d73d8
intel <intel@LAPTOP-HP>
2025-12-02 14:22:09 +0530

    Add project files.

---
+++ DOTNET PROGRAMS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace mycode
+{
+    class program
+    {
+        static void Main()
+        {
+            string transactionID = "ABCDEF234245";
+            Console.WriteLine("The transaction ID is:{0}",transactionID);
+            decimal amount = 7986666.67M;
+            Console.WriteLine("The amount is:{0}",amount);
+            bool isInternational = true;
+            Console.WriteLine(isInternational);
+            float customerrating = 3.0f;
+            Console.WriteLine("The Customer rating is:{0}", customerrating);
+            DateTime transactiontimestamp = DateTime.Now;
+            Console.WriteLine("The transaction timestamp is:{0}", transactiontimestamp);
+            long rewardpoints = 66677776;
+            Console.WriteLine("The rewardpoints is:{0}", rewardpoints);
+
+
+
+        }
+    }
+}
+
