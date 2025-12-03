2e0dee1038718cdead0ada7b01daf11f98d0d5e1
Jananisree <167199194+Jananisreesekar@users.noreply.github.com>
2025-12-03 20:55:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DeviceMonitor
+{
+    class TemperatureModule
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature Module is running");
+            VibrationModule.vibrationcheck();
+        }
+        class VibrationModule
+        {
+            public static void vibrationcheck()
+            {
+                Console.WriteLine("Vibration Module is running");
+             
+            }
+        }
+    }
+}
+               
+
+    
